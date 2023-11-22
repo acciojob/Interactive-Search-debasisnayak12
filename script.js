d516b34f9c3ca23e934ff4281566da218473054a
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-22 19:51:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+  const searchContainer = document.querySelector('.search');
+  const searchInput = document.querySelector('.input');
+  const searchBtn = document.querySelector('.btn');
+
+searchBtn.addEventListener(('click'), () => {
+	searchContainer.classList.add('active');
+	searchInput.focus();
+});
+
+
